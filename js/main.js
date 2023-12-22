fc4d8c9e569fb40f0e505c7cf2502fde6bd063fd
Juliano Silveira <108852825+iJulians@users.noreply.github.com>
2023-12-22 14:30:03 -0300

    Add corousel in tabletsmain.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 carousel = document.querySelector('.cards')
+cards = document.querySelectorAll('.card')
 let degValue = 0
+let wDevice = window.innerWidth
 function rotate(idButton) {
-    if(idButton.id == 'prev'){
-     degValue = degValue - 120;   
-    } else {
-        degValue = degValue + 120;
-    }
 
-    carousel.style.webkitTransform = "rotateY(" + degValue + "deg)";
-    carousel.style.mozTransform = "rotateY(" + degValue + "deg)";
-    carousel.style.oTransform = "rotateY(" + degValue + "deg)";
-    carousel.style.transform = "rotateY(" + degValue + "deg)";
+    if (wDevice >= 1024){
+        
+        if(idButton.id == 'prev'){
+            degValue = degValue - 120;   
+        } else {
+            degValue = degValue + 120;
+        }
+        
+        carousel.style.webkitTransform = "rotateY(" + degValue + "deg)";
+        carousel.style.mozTransform = "rotateY(" + degValue + "deg)";
+        carousel.style.oTransform = "rotateY(" + degValue + "deg)";
+        carousel.style.transform = "rotateY(" + degValue + "deg)";
+    } else {
+
+        if(idButton.id == 'prev'){
+            degValue -= 100;
+        } else {
+            degValue += 100;
+        }
+
+        if (degValue > 200) {
+            degValue = 0;
+        } else if(degValue < 0) {
+            degValue = 200;
+        }
+
+
+        cards.forEach(card => {
+            card.style.transform = 'translateX(-' + degValue + '%)';
+        });
+        
+    }
 }
